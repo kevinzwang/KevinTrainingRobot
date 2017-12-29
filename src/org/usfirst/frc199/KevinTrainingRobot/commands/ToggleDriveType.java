6aa3670159bc8346b91cc2ebad3ae817dcc54ee7
Kevin Wang <20215378+kevinzwang@users.noreply.github.com>
2017-12-28 17:44:10 -0800

    fixed command

--- src/org/usfirst/frc199/KevinTrainingRobot/commands/ToggleDriveType.java
+++ src/org/usfirst/frc199/KevinTrainingRobot/commands/ToggleDriveType.java
@@ -25,12 +25,12 @@ public class ToggleDriveType extends Command {
     }
 
     // Called just before this Command runs the first time
-    protected void initialize() {
-    		drivetrain.toggleDriveType();
+    protected void initialize() {	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    		drivetrain.toggleDriveType();
     }
 
     // Make this return true when this Command no longer needs to run execute()
